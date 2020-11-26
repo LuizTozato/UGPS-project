c18021b418dec595960f812286b67bb82f327727
Luiz Felipe Neves Tozato <lfntozato@gmail.com>
2020-11-26 18:09:26 -0300

    Exception handling - update

--- src/main/java/com/ugps/curso/services/UserService.java
+++ src/main/java/com/ugps/curso/services/UserService.java
@@ -3,6 +3,8 @@ package com.ugps.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +49,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id); //getOne não pega o objeto mas deixa ele em monitoramento.
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id); //getOne não pega o objeto mas deixa ele em monitoramento.
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
