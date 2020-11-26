392de0222954e6a076ad6494bc484c97bc804711
Luiz Felipe Neves Tozato <lfntozato@gmail.com>
2020-11-26 18:02:22 -0300

    Exception handling - delete

--- src/main/java/com/ugps/curso/services/UserService.java
+++ src/main/java/com/ugps/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ugps.curso.entities.User;
 import com.ugps.curso.repositories.UserRepository;
+import com.ugps.curso.services.exceptions.DatabaseException;
 import com.ugps.curso.services.exceptions.ResourceNotFoundException;
 
 //registramos a classe como Component para que ela seja um componente do Spring e assim seja injetado automaticamente com autowired
@@ -34,7 +37,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
